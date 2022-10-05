4ce84353fc491d276df644a513c03777cfe18c2e
kimierik <kimi.malkamaki@gmail.com>
2022-10-05 13:55:01 +0300

    restructure

--- addon-wasm/src/lib.rs
+++ addon-wasm/src/lib.rs
@@ -1,91 +1,169 @@
+use std::collections::HashMap;
 
 use wasm_bindgen::prelude::*;
 
+
+
+
+
 #[wasm_bindgen]
-pub fn convertfromto()->f32{
-    return 3.0 as f32
+pub struct App{
+    is_distance:bool,
+    conversion_table:HashMap<String,f32>,
+    window:web_sys::Window,
 }
 
 #[wasm_bindgen]
-pub fn main(from:String,to:String,value:f32)->f32{
-//turn value to meters first then turn it to whatever val we need
-    let mut convert:f32=1.0;
+impl App{
+    #[wasm_bindgen(constructor)]
+    pub fn new()->Self{
+        App{
+        is_distance:true,
+        window:web_sys::window().expect("nowindow"),
+        conversion_table:HashMap::from([
+            //x*f32 = meter
+            ("meter".to_string(),1.0),
+            ("mile".to_string(),1609.34),
+            ("inch".to_string(),0.0254),
+            ("Kilometer".to_string(),1000.0),
+            ("feet".to_string(),0.3048),
 
-    if from.eq(&String::from("m")){
-        convert*=1.0;
-    }
-    if from.eq(&String::from("mile")){
-        convert*=1609.34;
-    }
-    if from.eq(&String::from("inch")){
-        convert*=0.0254;
-    }
-    if from.eq(&String::from("Km")){
-        convert*=1000.0;
-    }
-    if from.eq(&String::from("feet")){
-        convert*=0.3048;
-    }
-
-
-    if from.eq(&String::from("Kilo")){
-        convert*=1.0;
-    }
-    if from.eq(&String::from("gram")){
-        convert*=0.001;
+            //x*f32=kg
+            ("Kilogram".to_string(),1.0),
+            ("gram".to_string(),0.001),
+            ("pound".to_string(),0.4536),
+            ("ton".to_string(),907.18),
+            ("ounce".to_string(),0.02835),
+        ])
+        }
     }
 
-    if from.eq(&String::from("pound")){
-        convert*=0.4536;
-    }
-    if from.eq(&String::from("ton")){
-        convert*=907.18
-    }
-    if from.eq(&String::from("ounce")){
-        convert*=0.02835;
+
+    pub fn changetxt(&self){
+        let document=self.window.document().expect("no document from windo");
+        let val= document.get_element_by_id("title");
+        val.unwrap().set_class_name("hide");
     }
 
 
 
 
+    pub fn set_isdist(&mut self){
+        let document=self.window.document().expect("no document from windo");
+        self.is_distance=true;
+        document.get_element_by_id("fromval").unwrap().set_class_name("show");
+        document.get_element_by_id("toval").unwrap().set_class_name("show");
 
- 
-
-    if to.eq(&String::from("m")){
-        convert/=1.0;
-    }
-    if to.eq(&String::from("mile")){
-        convert/=1609.34;
-    }
-    if to.eq(&String::from("inch")){
-        convert/=0.0254;
-    }
-    if to.eq(&String::from("Km")){
-        convert/=1000.0;
-    }
-    if to.eq(&String::from("feet")){
-        convert/=0.3048;
-    }
+        document.get_element_by_id("fromvalW").unwrap().set_class_name("hide");
+        document.get_element_by_id("tovalW").unwrap().set_class_name("hide");
 
 
-    if to.eq(&String::from("Kilo")){
-        convert/=1.0;
     }
-    if to.eq(&String::from("gram")){
-        convert/=0.001;
+    pub fn set_isweigth(&mut self){
+        let document=self.window.document().expect("no document from windo");
+        self.is_distance=false;
+        document.get_element_by_id("fromval").unwrap().set_class_name("hide");
+        document.get_element_by_id("toval").unwrap().set_class_name("hide");
+
+        document.get_element_by_id("fromvalW").unwrap().set_class_name("show");
+        document.get_element_by_id("tovalW").unwrap().set_class_name("show");
     }
-    if to.eq(&String::from("pound")){
-        convert/=0.4536;
+    pub fn get_isdist(&self)->bool{
+        self.is_distance
     }
-    if to.eq(&String::from("ton")){
-        convert/=907.18
+
+    pub fn convert_from_table(&self,from:String,to:String,value:f32)->f32{
+        let a=self.conversion_table.get(&from);
+        let b=self.conversion_table.get(&to);
+        return value*a.unwrap()/b.unwrap();
     }
-    if to.eq(&String::from("ounce")){
-        convert/=0.02835;
-    }
+
+    /*
+    pub fn convert_fn(&self,from:String,to:String,value:f32)->f32{
+        //turn value to meters first then turn it to whatever val we need
+        let mut convert:f32=1.0;
+
+        if from.eq(&String::from("m")){
+            convert*=1.0;
+        }
+        if from.eq(&String::from("mile")){
+            convert*=1609.34;
+        }
+        if from.eq(&String::from("inch")){
+            convert*=0.0254;
+        }
+        if from.eq(&String::from("Km")){
+            convert*=1000.0;
+        }
+        if from.eq(&String::from("feet")){
+            convert*=0.3048;
+        }
+
+
+        if from.eq(&String::from("Kilo")){
+            convert*=1.0;
+        }
+        if from.eq(&String::from("gram")){
+            convert*=0.001;
+        }
+
+        if from.eq(&String::from("pound")){
+            convert*=0.4536;
+        }
+        if from.eq(&String::from("ton")){
+            convert*=907.18
+        }
+        if from.eq(&String::from("ounce")){
+            convert*=0.02835;
+        }
 
 
 
-convert*value
+
+
+     
+
+        if to.eq(&String::from("m")){
+            convert/=1.0;
+        }
+        if to.eq(&String::from("mile")){
+            convert/=1609.34;
+        }
+        if to.eq(&String::from("inch")){
+            convert/=0.0254;
+        }
+        if to.eq(&String::from("Km")){
+            convert/=1000.0;
+        }
+        if to.eq(&String::from("feet")){
+            convert/=0.3048;
+        }
+
+
+        if to.eq(&String::from("Kilo")){
+            convert/=1.0;
+        }
+        if to.eq(&String::from("gram")){
+            convert/=0.001;
+        }
+        if to.eq(&String::from("pound")){
+            convert/=0.4536;
+        }
+        if to.eq(&String::from("ton")){
+            convert/=907.18
+        }
+        if to.eq(&String::from("ounce")){
+            convert/=0.02835;
+        }
+
+
+
+    convert*value
+    }
+*/
 }
+
+
+
+
 
--- script.js
+++ script.js
@@ -1,50 +1,40 @@
-import init,{main} from './addon-wasm/pkg/addon_wasm.js'
+import init,{App} from './addon-wasm/pkg/addon_wasm.js'
 await init();
+var app=new App();
+
 document.getElementById("output").textContent="output";
-let isDistance=true;
+
+app.set_isdist();
+
 
 function change_to_dist(){
-	//cahges the thing we use to distance
-	isDistance=true;
-document.getElementById("fromval").style.display="inline";
-document.getElementById("toval").style.display="inline";
-
-document.getElementById("fromvalW").style.display="none";
-document.getElementById("tovalW").style.display="none";
+	app.set_isdist();
 }
 
 
 function change_to_weigth(){
-
-	isDistance=false;
-document.getElementById("fromval").style.display="none";
-document.getElementById("toval").style.display="none";
-
-document.getElementById("fromvalW").style.display="inline";
-document.getElementById("tovalW").style.display="inline";
-	
+	app.set_isweigth();
 }
 
 
 
 function calcul(){
-//if is distance or something else
-	if (isDistance){
+	//if is distance or something else
+	if (app.get_isdist()){
 		var from=document.getElementById("fromval").value;
 		var to=document.getElementById("toval").value;
 	}else{
 		var from=document.getElementById("fromvalW").value;
 		var to=document.getElementById("tovalW").value;
 	}
-
-
+	//the above cannot be moved to wasm for now
 
 	var num = document.getElementById("input").value;
 
 	if (isNaN(num)){
 	document.getElementById("output").textContent="input txt please";
 	}else{
-	document.getElementById("output").textContent=main(from,to,num).toFixed(2);
+	document.getElementById("output").textContent=app.convert_from_table(from,to,num).toFixed(2);
 	}
 }
 
